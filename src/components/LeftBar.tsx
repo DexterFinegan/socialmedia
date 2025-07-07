43aa254618eeb7aa98f472d8c0a3d3b599433de7
Dexter Finegan <dexter.finegan@gmail.com>
2025-07-07 23:26:47 +0100

    Initial commit: start of project

---
+++ src/components/LeftBar.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const menuList = [
+    {
+        id: 1,
+        name: "Homepage",
+        link: "/",
+        icon: "home.svg",
+    },
+    {
+        id: 2,
+        name: "Explore",
+        link: "/",
+        icon: "explore.svg",
+    },
+    {
+        id: 3,
+        name: "Notification",
+        link: "/",
+        icon: "notification.svg",
+    },
+    {
+        id: 4,
+        name: "Messages",
+        link: "/",
+        icon: "messages.svg",
+    },
+    {
+        id: 5,
+        name: "Bookmarks",
+        link: "/",
+        icon: "bookmarks.svg",
+    },
+    {
+        id: 6,
+        name: "Profile",
+        link: "/",
+        icon: "profile.svg",
+    },
+    {
+        id: 7,
+        name: "More",
+        link: "/",
+        icon: "more.svg",
+    }
+]
+
+
+const LeftBar = () => {
+    return (
+        <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+            {/*Logo Menu Button*/}
+            <div className="flex flex-col gap-4 text-m items-center xl:items-start">
+                {/*Logo*/}
+                <Link href="/" className="p-2 rounded-full hover:bg-[#181818]">
+                <Image src="icons/logo.svg" alt="logo" width={24} height={24}/>
+                </Link>
+                {/*Menu List*/}
+                <div className="flex flex-col gap-3">
+                    {menuList.map(item => (
+                        <Link href={item.link} className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4" key={item.id}>
+                            <Image src={`icons/${item.icon}`} alt={item.name} width={24} height={24}/>
+                            <span className="hidden xl:inline">{item.name}</span>
+                        </Link>
+                    ))}
+                </div>
+                {/*Button*/}
+                <Link href="/" className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center xl:hidden">
+                    <Image src="icons/post.svg" alt="post" width={24} height={24}/>
+                </Link>
+                <Link href="/" className="hidden xl:block bg-white text-black rounded-full font-bold py-2 px-20">Post</Link>
+            </div>
+            {/*User*/}
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <div className="w-10 h-10 relative rounder-full overflow-hidden">
+                        <Image src="/general/avatar.png" alt="DexUp" fill/>
+                    </div>
+                    <div className="hidden xl:flex flex-col">
+                        <span className="font-bold">DexUp</span>
+                        <span className="text-sm text-textGray">@DexUp</span>
+                    </div>
+                </div>
+                <div className="hidden xl:block cursor-pointer font-bold">...</div>
+            </div>
+        </div>
+    )
+}
+
+export default LeftBar
